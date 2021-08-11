1d56580c2cd7c5813bb258f07b3cef8ca4c1a1a6
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2021-08-11 11:48:18 +0200

    Created const.php file

---
+++ lib/const.php
@@ -0,0 +1,16 @@
+<?php
+$_LIVERY = ['Mercedes' => '#00d2be',
+'Red Bull' => '#0600ef',
+'McLaren' => '#ff8700',
+'Ferrari' => '#dc0000',
+'Aston Martin' => '#006F62',
+'Alpine' => '#0090FF',
+'AlphaTauri' => '#2B4562',
+'Alfa Romeo' => '#900000',
+'Williams' => '#005AFF',
+'Haas' => '#FFFFFF'];
+
+$DEFAULT_DATETIME_FORMAT = 'd/m/Y';
+$REVERSED_DATETIME_FORMAT = 'Y/m/d';
+
+?>
