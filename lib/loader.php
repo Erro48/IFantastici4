22f267fdbc91c8c3ad718bfebb55ff73cc1465d2
Enrico Tagliaferri <errotagliaferri.4801@gmail.com>
2021-09-19 11:34:22 +0200

    Created loader file

---
+++ lib/loader.php
@@ -0,0 +1,14 @@
+<script>
+    function removeLoader() {
+        $('.loader').fadeOut(1000);
+        $('.container-fluid').fadeIn(1000);
+    }
+</script>
+
+<div class="loader-container">
+    <div class="loader row justify-content-center">
+    <div id="firstLoader" class="col-3"></div>
+    <div id="secondLoader" class="col-3"></div>
+    <div id="thirdLoader" class="col-3"></div>
+    </div>
+</div>
